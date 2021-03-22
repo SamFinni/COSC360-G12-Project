ca610bc15e7b85f8795f7a46c6e8bab6c69a68ef
Tasha <tkuch97@gmail.com>
2021-03-22 11:51:35 -0700

    add posts and postscores tables

---
+++ backend/models/post.model.js
@@ -0,0 +1,39 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../db/sequelize');
+
+// modifying the schema below will modify the database schema
+const Post = sequelize.define('post', {
+    uid: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'users',
+            key: 'id'
+        },
+    },
+    title: {
+        type: DataTypes.STRING,
+    },
+    body: {
+        type: DataTypes.STRING,
+    },
+    tags: {
+        type: DataTypes.STRING,
+    },
+    createdAt: { // default field, but we want to customize it
+        type: DataTypes.DATE(3),
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)'),
+    },
+    updatedAt: { // default field, but we want to customize it
+        type: DataTypes.DATE(3),
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)'),
+    },
+});
+
+// automatically creates the database table if it doesn't exist (with letter 's' appended)
+// modifies schema if table exists and schema above was updated
+Post.sync({
+    alter: true,
+});
+
+module.exports = Post;
---
+++ backend/models/postscore.model.js
@@ -0,0 +1,41 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../db/sequelize');
+
+// modifying the schema below will modify the database schema
+const PostScore = sequelize.define('postscore', {
+    pid: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'posts',
+            key: 'id'
+        },
+    },
+    uid: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'users',
+            key: 'id'
+        },
+    },
+    score: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    vote: {
+        type: DataTypes.INTEGER,
+    },
+    createdAt: { // default field, but we want to customize it
+        type: DataTypes.DATE(3),
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)'),
+    },
+    updatedAt: { // default field, but we want to customize it
+        type: DataTypes.DATE(3),
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)'),
+    },
+});
+
+PostScore.sync({
+    alter: true,
+});
+
+module.exports = PostScore;
