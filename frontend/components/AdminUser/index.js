2b9ba134adac0e421e21540346528d98f002bbc9
Matt Borle <43189368+MattB70@users.noreply.github.com>
2021-04-07 14:55:20 -0700

    dynamic buttons

--- frontend/components/AdminUser/index.js
+++ frontend/components/AdminUser/index.js
@@ -4,24 +4,21 @@ import styles from '../../styles/components/AdminUser.module.css';
 export default function AdminUser({ data }) {
 
     // Borders
-    const defaultBorderColor = "3px outset #b7cecd"; // default border colour for when user is not admin or disabled
-    const adminBorderColor = "3px outset #adfbad"; // border colour for when user is admin
-    const disabledBorderColor = "3px outset #ff9966"; // border colour for when user is disabled
+    const defaultBorderColor = "2px outset #b7cecd"; // default border colour for when user is not admin or disabled
+    const adminBorderColor = "2px outset #adfbad"; // border colour for when user is admin
+    const disabledBorderColor = "2px outset #ff9966"; // border colour for when user is disabled
     var currentBorderColor = defaultBorderColor; // current border color begins as default
     // Backgrounds
     const defaultBackgroundColor = "#e9ebf0"; // default background colour for when user is not admin or disabled
     const adminBackgroundColor = "#d3f2dc"; // background colour for when user is admin
     const disabledBackgroundColor = "#f0d9c9"; // background colour for when user is disabled
     var currentBackgroundColor = defaultBackgroundColor; // current background color begins as default
-    // Admin crown
-    const Adminpic = "crown.png";
-    const AdminTitle = "Admin";
-    // Disabled crown
-    const Disabledpic = "cross.png";
-    const DisabledTitle = "Disabled";
     // Picture beside Username
     var splashPic = "";
     var splashTitle = "";
+    // Button text
+    var adminButtonText = "Admin";
+    var disableButtonText = "Disable";
 
     var disabledValue = false;
     var adminValue = false;
@@ -31,7 +28,7 @@ export default function AdminUser({ data }) {
         if(confirm("Are you sure you wish to disable the user with ID = " + data.id + "?"))
         {
             // disable user
-            alert("disable user Confirmed!");
+            window.location.reload();
         }
     }
     function adminChange(value){
@@ -39,7 +36,7 @@ export default function AdminUser({ data }) {
         if(confirm("Are you sure you wish to give administrator status to the user with ID = " + data.id + "?"))
         {
             // admin user
-            alert("admin user Confirmed!");
+            window.location.reload();
         }
     }
 
@@ -47,15 +44,17 @@ export default function AdminUser({ data }) {
     {
         currentBorderColor = adminBorderColor;
         currentBackgroundColor = adminBackgroundColor;
-        splashPic = Adminpic;
-        splashTitle = AdminTitle;
+        splashPic = "crown.png";
+        splashTitle = "Admin";
+        adminButtonText = "Revoke Admin";
     }
     if(data.disabled)
     {
         currentBorderColor = disabledBorderColor;
         currentBackgroundColor = disabledBackgroundColor;
-        splashPic = Disabledpic;
-        splashTitle = DisabledTitle;
+        splashPic = "cross.png";
+        splashTitle = "Disabled";
+        disableButtonText = "Enable";
     }
 
     return (
@@ -75,7 +74,7 @@ export default function AdminUser({ data }) {
                     <button className={styles.button} // Toggle button for disabled value
                         onClick={()=> disabledChange(!disabledValue)}
                         name="disabled"
-                    ><span>Toggle Disabled</span></button>
+                    ><span>{disableButtonText}</span></button>
                 </td>
 
             </tr>
@@ -91,14 +90,14 @@ export default function AdminUser({ data }) {
                     <button className={styles.button} // Toggle button for admin value
                         onClick={()=> adminChange(!adminValue)}
                         name="admin"
-                    ><span>Toggle Admin</span></button>
+                    ><span>{adminButtonText}</span></button>
                 </td>
             </tr>
             
         </table>
             <hr className={styles.separator}></hr>
             <p className={styles.date}>ID: <span className={styles.emphasis}>{data.id}</span> &nbsp; Created: {data.createdAt.substring(0,10)}</p>
-            <a className={styles.link} href={`/Profile/${data.id}`} target="_blank"><span>View Profile</span></a>
+            <a className={styles.link} href={`/Profile/${data.id}`} target="_blank"><span>Profile</span></a>
       </div>
     </div>
     );
