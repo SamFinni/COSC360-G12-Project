662a341bfe89fc195b46619720d001a3a48e0d9e
SamFinni <sfinnigan@gmx.com>
2021-04-10 23:48:35 -0700

    made friends page use live data now. added friend request section to friends page, with functional accept/deny buttons. added polling to notification component

--- frontend/pages/friends/index.js
+++ frontend/pages/friends/index.js
@@ -1,19 +1,21 @@
 import Head from 'next/head';
-import axios from 'axios';
 import dynamic from "next/dynamic";
 import styles from '../../styles/pages/FriendsPage.module.css';
 import Footer from '../../components/Footer';
-import Friend from '../../components/Friend';
 const Header = dynamic(() => import('../../components/Header'), {
   ssr: false
 });
 const Navbar = dynamic(() => import('../../components/Navbar'), {
   ssr: false
 });
-import * as cfg from '../../config';
-const backend = 'http://' + cfg.BACKEND_IP + ':' + cfg.BACKEND_PORT;
+const FriendList = dynamic(() => import('../../components/FriendList'), {
+  ssr: false
+});
+const RequestList = dynamic(() => import('../../components/RequestList'), {
+  ssr: false
+});
 
-export default function FriendsPage(props) {
+export default function FriendsPage() {
   return (
     <div className={styles.page}>
       <Head>
@@ -27,26 +29,11 @@ export default function FriendsPage(props) {
 
       <div className={styles.container}>
         <h1 className={styles.title}>Friends</h1>
-        <div className={styles.friendlist}>
-          {props.friends.map((friend, idx) => (
-            <Friend key={`friend-${idx}`} data={friend} />
-          ))}
-        </div>
+        <FriendList />
+        <RequestList />
       </div>
 
       <Footer />
     </div>
   )
-}
-
-export async function getStaticProps() {
-  const res = await axios.post(backend + '/friend/list', {
-    uid: 2
-  });
-
-  return {
-    props: {
-      friends: res.data.list
-    }
-  }
 }
