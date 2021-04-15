9ce62da14d0907d3f247dff81a9023be1267f8ec
Danielle Macdonald <danielle.macdonald@alumni.ubc.ca>
2021-04-15 15:23:07 -0700

    update link

--- frontend/components/Friend/index.js
+++ frontend/components/Friend/index.js
@@ -4,7 +4,7 @@ import styles from '../../styles/components/Friend.module.css';
 export default function Friend({ data }) {
   const imgSource = data.image ?? '/user.png';
   return (
-    <Link href={`/user/${data.id}`}>
+    <Link href={`/viewprofile/${data.id}`}>
       <div className={styles.container}>
         <div className={styles.user}>
           <img className={styles.pic} src={imgSource} />
